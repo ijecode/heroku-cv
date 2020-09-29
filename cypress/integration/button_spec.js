e05cf5aaa4f7bbb475a5550e28d039c2cdfdff89
Ian Edgeley <ianedgeley@gmail.com>
2020-09-29 16:22:33 +0100

    Update button_spec.js

--- cypress/integration/button_spec.js
+++ cypress/integration/button_spec.js
@@ -2,7 +2,7 @@
 
 describe('Button Tester', () => {
     it('Test Passed!', () => {
-      cy.visit('http://localhost:5000/index.html')
+      cy.visit('http://localhost:5000')
       cy.get('button').click()
       cy.contains('ianedgeley@gmail.com').should('have.css', 'display', 'block')
 
